af87ca9d6227ad817c2588d3904047c1777822da
saturnworld <1033227298@qq.com>
2018-09-04 22:45:25 +0800

    使用postman测试获取用户数量和姓名

--- mall/apps/users/views.py
+++ mall/apps/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
+
+
+class RegisterUsernameCountAPIView(APIView):
+    """
+    对接收到的用户名进行验证
+    """
+    def get(self,request,username):
+        #获取用户个数
+        count = User.objects.filter(username=username).count()
+
+        context = {
+            'count':count,
+            'username':username
+        }
+        return Response(context)
